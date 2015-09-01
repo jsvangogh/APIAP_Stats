b0460d61c16ba1f45e5221b047932bbe34477853
jrvangogh <jrvangogh@gmail.com>
2015-08-31 23:23:22 -0700

    more PEP8

--- put_matches_in_dataframe.py
+++ put_matches_in_dataframe.py
@@ -5,7 +5,7 @@ Created on Wed Aug 19 12:51:57 2015
 @author: Innerfunk
 '''
 
-#%% Initialize
+# Initialize
 
 import json
 import pandas as pd
@@ -31,34 +31,39 @@ COLUMNS = ['match id', 'patch', 'queue', 'region']
 KEY = 'PUT RIOT API KEY HERE'
 rawpi.set_api_key(KEY)
 
-#%% Define DataFrame Creation Method
+# Define DataFrame Creation Method
+
+
 def put_matches_in_dataframe(patch, queue_type, region):
-    filename = TOP_FOLDER + '/' + patch + '/' + queue_type + '/' + region + '.json'
+    filename = TOP_FOLDER + '/' + patch + '/' + queue_type + \
+               '/' + region + '.json'
     print(filename)
-    
+
     with open(filename) as data_file:
         data = json.loads(data_file.read())
-        
+
     matches_frame = pd.DataFrame(index=range(len(data)), columns=COLUMNS)
     queue = QUEUE_DICT[queue_type]
     matches_frame['match id'] = data
     matches_frame['patch'] = patch
     matches_frame['queue'] = queue
     matches_frame['region'] = REGION_DICT[region]
-    
+
     return matches_frame
-    
-#%% Get All File into One Frame and Save
+
+# Get All File into One Frame and Save
 frame_list = []
-for match_properties in [(patch, queue_type, region) for patch in PATCHES \
-for queue_type in QUEUE_DICT.keys() for region in REGIONS]:
+for match_properties in [(patch, queue_type, region) for patch in PATCHES
+                         for queue_type in QUEUE_DICT.keys()
+                         for region in REGIONS]:
     frame_list.append(put_matches_in_dataframe(*match_properties))
 
 all_frames = pd.concat(frame_list, ignore_index=True)
 
-#%% Filter Matches
+# Filter Matches
 region_frames = all_frames[all_frames['region'].isin([1, 2, 3, 4])]
 region_ranked_frames = region_frames[region_frames['queue'] == 'ranked']
+
 
 all_frames.to_csv(ALL_SAVE_FILENAME, index=False)
 region_ranked_frames.to_csv(FILTERED_SAVE_FILENAME, index=False)
