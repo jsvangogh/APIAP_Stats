1fc5dc80759ea06a17fa38dc27078a0bfdc680fc
jrvangogh <jrvangogh@gmail.com>
2015-08-31 19:24:08 -0700

    Changed Name/Functionality of Methods
    Removed 'other' option from bar-graph-making methods. Not used or really
    useful. Methods more explicitly state they are for graphing counts of a
    column's values.

--- make_figures.py
+++ make_figures.py
@@ -10,96 +10,155 @@ import pandas as pd
 from plotly import tools
 from plotly.graph_objs import *
 
-# Define Methods for Easy Figure-Creating
+
+# Define Methods for Easy Figure-Creating 
+# Using Both Column Value Counts and Item Column Sums
 
 
-def make_label_value_tuple(frame, value_col, other=True, 
-                           other_threshold=.02, normalize=False, total=1):
+def make_data_tuple_value_counts(frame, value_col, normalize=False,
+                                 by_group=True, total=1):
     """
-    Given a frame, makes a tuple of labels to values.
-    Can group small representations into an 'other' group and normalize values
-    on a total argument.
+    Given a frame, makes a tuple of labels to values for a column's value 
+    counts. Can group small representations into an 'other' group and normalize 
+    values on a total argument.
     
     Keyword arguments:
     frame -- the DataFrame containing values
     value_col -- column name for values to plot
-    other -- whether to group small representations into 'other' group
-             Default - True
-    other_threshold -- threshold for which smaller values will be grouped into
-                       'other' group. Default - 0.02
-    normalize -- whether to normalize values to sum to 1, Default - True
+    normalize -- whether to normalize values to sum to 100, Default - True
+    by_group -- when normalizing, should each group sum to 100, Default - True
     total -- total size of data set, used for normalizing
     """
     val_counts = frame[value_col].value_counts().to_dict()
-    if other:
-        val_entries = len(frame)
-        keys = [k for k in val_counts.keys() 
-                if val_counts[k]/val_entries >= other_threshold]
-        other_sum = val_entries - sum([val_counts[k] for k in keys])
-        val_counts['other'] = other_sum
-        keys.append('other')
-    else:
-        keys = val_counts.keys()
+    keys = val_counts.keys()
     
     label = [a for a in keys]
     if normalize:
-        value = [val_counts[a]/total for a in keys]
+        if by_group:
+            total=len(frame)
+        value = [val_counts[a]/total*100 for a in keys]
     else:
         value = [val_counts[x] for x in keys]
     
     return (label, value)
     
 
-def make_bar(
-        frame, value_col, stack_col,
-        other=True, other_threshold=.02, normalize=False,
-        filename='bar graph',
-        layout=Layout(barmode='stack')):
+def make_bar_value_counts(
+        frame, value_col, split_col, normalize=False, by_group=True,
+        filename='bar graph', layout=Layout(barmode='stack')):
     """
-    Makes a bar graph with grouped data from a DataFrame
+    Makes a bar graph with grouped data from a DataFrame using a column's
+    value counts
     
     Keyword arguments:
     frame -- the DataFrame containing values
     value_col -- column name for values to plot
-    stack_col -- column name that groups are split on
-    other -- whether to group small representations into 'other' group
-             Default - True
-    other_threshold -- threshold for which smaller values will be grouped into
-                       'other' group. Default - 0.02
+    split_col -- column name that groups are split on
     normalize -- whether to normalize values to sum to 1, Default - True
+    by_group -- when normalizing, should each group sum to 100, Default - True
     filename -- name of plotly file
     layout -- layout of figure, Default - stacked bar graph
     """
             
-    stacks = list(pd.Series(frame[stack_col].ravel()).unique())
+    groups = list(pd.Series(frame[split_col].ravel()).unique())
     total=len(frame)
     
     traces = []
-    for stack in sorted(stacks):
-        stack_frame = frame[frame[stack_col] == stack]
+    for group in sorted(groups):
+        stack_frame = frame[frame[split_col] == group]
         
-        (x, y) = make_label_value_tuple(frame=stack_frame,
-                                        value_col=value_col, 
-                                        normalize=normalize, total=total)        
+        (x, y) = make_label_value_tuple(frame=stack_frame, value_col=value_col,
+            normalize=normalize, by_group=by_group, total=total)        
         
-        name = str(stack)
+        name = str(group)
             
         traces.append(Bar(x=x, y=y, name=name))
     figure = Figure(data=traces, layout=layout)
     
-    url = py.plot(figure, filename=(filename))
-    
+    return py.plot(figure, filename=(filename))
 
-# stuff
 
+# make figures
+
+# Graphs for AP Items Purchased by Lane
 stats = pd.read_csv('stats.csv')
 for patch in [5.11, 5.14]:
     f = stats[stats['patch'] == patch]
-    make_bar(
-        frame=f[f['num AP items'] >= 1], filename=str(patch),
-        value_col='item 1', stack_col='lane', normalize=True,
+    plot_title = 'First AP Item Purchased by Lane, Patch {0}'.format(patch)
+    make_bar_value_counts(
+        frame=f[f['num AP items'] >= 1], filename=plot_title,
+        value_col='item 1', split_col='lane', normalize=True, by_group=False,
         layout=Layout(barmode='stack', 
-                      yaxis=YAxis(range=[0, 0.18], title='Buy Percentage'),
-                      title='First Item Purchased {0}'.format(patch)
-               )
-                     )
+                      yaxis=YAxis(range=[0, 18], title='Buy Percentage'),
+                      title=plot_title
+        )
+    )
+
+# Graphs for Item Purchases for First, Second, and Third Purchases                     
+for num in range(1, 6):
+    f = stats
+    plot_title = 'AP Item #{0} Purchased by Patch'.format(num)
+    value_col = 'item {0}'.format(num)
+    make_bar_value_counts(
+        frame=f[f[value_col] != '0'], 
+        filename=plot_title,
+        value_col=value_col, split_col='patch', 
+        normalize=True, other=False,
+        layout=Layout(barmode='group', 
+                      yaxis=YAxis(title='Buy Percentage'),
+                      title=plot_title
+        )
+    )
+            
+# Graphs Showing the Percentage of #-item Builds That Contain Each Item
+FULL_AP_ITEMS = ['Abyssal Scepter',
+                 'Archangel\'s Staff',
+                 'Athene\'s Unholy Grail',
+                 'Haunting Guise',
+                 'Liandry\'s Torment',
+                 'Lich Bane',
+                 'Luden\'s Echo',
+                 'Mejai\'s Soulstealer',
+                 'Morellonomicon',
+                 'Nashor\'s Tooth',
+                 'Rabadon\'s Deathcap',
+                 'Rod of Ages',
+                 'Rylai\'s Crystal Scepter',
+                 'Seraph\'s Embrace',
+                 'Void Staff',
+                 'Will of the Ancients',
+                 'Zhonya\'s Hourglass']
+
+                 
+for i in range(2,7):
+    traces=[]
+    i_frame = stats[(stats['num AP items'] == i)]
+    
+    for patch in [5.11, 5.14]:
+        f = i_frame[i_frame['patch'] == patch]
+        total = len(f)
+        x = FULL_AP_ITEMS
+        y = [f[l].sum()/total*100 for l in x]
+        traces.append(Bar(x=x, y=y, name=str(patch)))
+        
+        # Make Graphs that Separate by Lane
+        lane_plot_title = 'Percentage of {0}-AP-Item Builds Each Item Present In By Lane, Patch {1}'.format(i, patch)
+        lane_traces = []
+        for lane in ['bottom', 'jungle', 'middle', 'top']:
+            lane_f = f[f['lane'] == lane]
+            lane_y = [lane_f[l].sum()/total*100 for l in x]
+            lane_traces.append(Bar(x=x, y=lane_y, name=lane))
+        lane_layout=Layout(barmode='stack', 
+            yaxis=YAxis(title='Percentage of Builds Item is Present In'),
+            title=lane_plot_title)
+        lane_figure = Figure(data=lane_traces, layout=lane_layout)
+        py.plot(lane_figure, filename=lane_plot_title)
+    
+    plot_title = 'Percentage of {0}-AP-Item Builds Each Item Present In'.format(i)
+    layout=Layout(barmode='group', 
+      yaxis=YAxis(title='Percentage of Builds Item is Present In'),
+      title=plot_title)
+        
+    figure = Figure(data=traces, layout=layout)
+    
+    py.plot(figure, filename=(plot_title))
